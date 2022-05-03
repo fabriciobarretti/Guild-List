05c01324fdaa6682696dd0e04b218e8dc5880dba
Fabrício Barretti <fabriciobarretti@gmail.com>
2022-05-03 17:08:58 -0300

    Added guild pages
    Now you can click on the guild name on the list and it'll show you all the guild info.

--- index.js
+++ index.js
@@ -23,11 +23,43 @@ let addGuildBtn = document.getElementById("add-guild-btn");
 
 let guildListHolder = ``;
 // console.log(guildList)
-
+let guildPage = [];
 let guildList = JSON.parse(localStorage.getItem("guildList"));;
 // console.log(localStorage.getItem("guildList"));
 
 let listContentEl = document.getElementById("show-content");
+
+// Get the guild name from the URL
+let pageURL = window.location.search;
+let urlParam = new URLSearchParams(pageURL);
+let guildURL = urlParam.get("guild");
+console.log(guildURL);
+
+// This "if-else" at the very beginning is definitely not a good practice. 
+if (guildURL !== null){
+	let guildExists = false;
+
+	for (let i = 0; i<guildList.length; i++){
+		if (guildURL.toLowerCase() == guildList[i].name.toLowerCase()){
+			let status;
+
+			if (guildList[i].isActive === true){
+				status = `Active`
+			} else {
+				status = `Inactive`
+			}
+
+			listContentEl.innerHTML = `
+			<p><b>Guild name:</b> ${guildList[i].name}</p>
+			<p><b>Game:</b> ${guildList[i].game}</p>
+			<p><b>Number of Players:</b> ${guildList[i].size}</p>
+			<p><b>Status:</b> ${status}</p>
+			`
+
+			guildExists = true;
+		}
+	}
+}
 
 async function renderList(){
 		listContentEl.innerHTML = ``;
@@ -41,15 +73,19 @@ async function renderList(){
 			} else {
 				status = `Inactive`
 			}
-	
+
 			guildListHolder += `
 				<tr>
-					<td>${guildList[i].name}</td>
+					<td><a id="${guildList[i].name}-guild" href="./index.html?guild=${guildList[i].name}">${guildList[i].name}</a></td>
 					<td>${guildList[i].game}</td>
 					<td>${guildList[i].size}</td>
 					<td>${status}</td>
 				</tr>
 			`
+					
+			// Trying to make the printed <td> clickable to its own object. I dropped it.
+			// guildPage[i] = document.getElementById(`${guildList[i].name}Guild`);
+			// console.log(guildPage[i]);
 		}
 	
 		listContentEl.innerHTML = `
@@ -63,9 +99,26 @@ async function renderList(){
 			</tr>
 			</thead>
 		`+ guildListHolder + `</table>`;
+
+		// The idea was to create .onclick functions to listen to clicks in every list item.
+
+		// let guildName = `${guildList[i].name}-guild` // Should store the name of the <a> tag that triggers the printing of the guild info.
+
+		// guildName.onclick = async function(){
+		// 	listContentEl.innerHTML = `
+		// 	<p><b>Guild name:</b> ${guildList[i].name}</p>
+		// 	<p><b>Game:</b> ${guildList[i].game}</p>
+		// 	<p><b>Number of Players:</b> ${guildList[i].size}</p>
+		// 	<p><b>Status:</b> ${status}</p>
+		// 	`
+		//}
 }
 
 showGuildBtn.onclick = async function(){
+	// if (guildURL){
+	// 	window.location.assign(`./index.html`);
+	// } 
+	
 	renderList();
 }
 
@@ -73,9 +126,6 @@ findGuildBtn.onclick = function(){
 	let guild = findGuildInput.value;
 	let guildExists = false;
 
-	// console.log(guild.toLowerCase());
-	// console.log(guildList[0].name.toLowerCase());
-	
 	for (let i = 0; i<guildList.length; i++){
 		if (guild.toLowerCase() == guildList[i].name.toLowerCase()){
 			let status;
@@ -117,10 +167,10 @@ findGuildInput.addEventListener("keypress", function(event) {
 
 addGuildBtn.onclick = async function (){
 	  listContentEl.innerHTML = `
-		<p>Guild name: <input type="text" id="guild-name-input"></p>
-		<p>Game: <input type="text" id="guild-game-input"></p>
-		<p>Number of Players: <input type="text" id="guild-size-input"></p>
-		<p>Status: <input type="text" id="guild-status-input"></p>
+		<p><b>Guild name</b>: <input type="text" id="guild-name-input"></p>
+		<p><b>Game</b>: <input type="text" id="guild-game-input"></p>
+		<p><b>Number of Players</b>: <input type="text" id="guild-size-input"></p>
+		<p><b>Status</b>: <input type="text" id="guild-status-input"></p>
 
 		<button id="add-btn">Add guild</button>
 	  `;
